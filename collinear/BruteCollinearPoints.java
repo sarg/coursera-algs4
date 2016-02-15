a5acd833c35da2d7534011a3cfee4f91e7c45250
Sergey Trofimov <sarg@sarg.org.ru>
2016-02-15 16:16:49 +0300

    add collinear points module

---
+++ collinear/BruteCollinearPoints.java
@@ -0,0 +1,23 @@
+/**
+ * Brute force. Write a program BruteCollinearPoints.java that examines 4
+ * points at a time and checks whether they all lie on the same line segment,
+ * returning all such line segments. To check whether the 4 points p, q, r,
+ * and s are collinear, check whether the three slopes between p and q,
+ * between p and r, and between p and s are all equal.
+ */
+public class BruteCollinearPoints {
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return -1;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return null;
+    }
+}
+
